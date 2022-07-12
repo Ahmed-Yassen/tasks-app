0a308b1c74aba92d36ea0b3c84bfb91885339e5c
Ahmed I. Yassen <ahmed.ibrahim.yassen@gmail.com>
2022-07-13 00:11:37 +0200

    Finish task route

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,8 +1,56 @@
 const express = require("express");
+const { body, param } = require("express-validator");
 const router = express.Router();
 const controller = require("../controllers/taskController");
 const authMW = require("../middlewares/authMW");
+const validationMW = require("../middlewares/validationMW");
 
-router.route("/tasks").post(authMW, controller.createTask);
+router
+  .route("/tasks")
+  .post(
+    authMW,
+    [
+      body("description")
+        .isString()
+        .withMessage("Task's description should be text!"),
+      body("isCompleted")
+        .optional()
+        .isBoolean()
+        .withMessage("Task's isCompleted should be either true of false!"),
+    ],
+    validationMW,
+    controller.createTask
+  )
+  .get(authMW, controller.getUserTasks);
 
+router
+  .route("/tasks/:id")
+  .patch(
+    authMW,
+    [
+      param("id")
+        .isMongoId()
+        .withMessage("Task's id should be a valid MongoID"),
+      body("description")
+        .optional()
+        .isString()
+        .withMessage("Task's description should be text!"),
+      body("isCompleted")
+        .optional()
+        .isBoolean()
+        .withMessage("Task's isCompleted should be either true of false!"),
+    ],
+    validationMW,
+    controller.updateUserTask
+  )
+  .delete(
+    authMW,
+    [
+      param("id")
+        .isMongoId()
+        .withMessage("Task's id should be a valid MongoID"),
+    ],
+    validationMW,
+    controller.removeTask
+  );
 module.exports = router;
