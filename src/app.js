82f7c622328f3237a30bf83390082af6e025cf54
Ahmed I. Yassen <ahmed.ibrahim.yassen@gmail.com>
2022-07-13 00:12:30 +0200

    Add task route to app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const userRoute = require("../routes/userRoute");
+const taskRoute = require("../routes/taskRoute");
+
 /********** Run Server ***********/
 const app = express();
 mongoose
@@ -17,6 +19,8 @@ app.use(express.json());
 
 /********** Routes ***********/
 app.use(userRoute);
+app.use(taskRoute);
+
 /********** Page not found ***********/
 app.use((req, res) => {
   res.status(404).json({ msg: "PAGE NOT FOUND!" });
