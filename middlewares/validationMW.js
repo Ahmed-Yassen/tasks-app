79216b579d91ceec3faed3f75e1ba5cd09fdc55e
Ahmed I. Yassen <ahmed.ibrahim.yassen@gmail.com>
2022-07-15 16:05:39 +0200

    Clean validationMW

--- middlewares/validationMW.js
+++ middlewares/validationMW.js
@@ -3,11 +3,12 @@ const { validationResult } = require("express-validator");
 module.exports = (req, res, next) => {
   const result = validationResult(req);
   if (!result.isEmpty()) {
-    const message = result.errors.reduce(
-      (current, error) => current + error.msg + " ",
+    let message = result.errors.reduce(
+      (accumlated, current) => accumlated + current.msg + " & ",
       ""
     );
-    let error = new Error(message);
+    message = message.slice(0, message.length - 3);
+    const error = new Error(message);
     error.status = 400;
     throw error;
   }
