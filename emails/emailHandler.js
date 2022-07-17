3d1f373f66936861061718de3487ea386389d9c9
Ahmed I. Yassen <ahmed.ibrahim.yassen@gmail.com>
2022-07-17 18:30:53 +0200

    Finish sendWelcomeEmail function

--- emails/emailHandler.js
+++ emails/emailHandler.js
@@ -4,3 +4,12 @@ const sendGridAPIKey =
   "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";
 
 sgMail.setApiKey(sendGridAPIKey);
+
+exports.sendWelcomeEmail = (userEmail, userName) => {
+  sgMail.send({
+    to: userEmail,
+    from: "ahmedibrahim9629@gmail.com",
+    subject: "Thanks for joining in!",
+    text: `Welcome to the app, ${userName}!\n\tLet me show you how to use the App:`,
+  });
+};
