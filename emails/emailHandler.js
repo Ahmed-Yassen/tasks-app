11bff16f1ee83308bec3dca46c67c64d48727d1c
Ahmed I. Yassen <ahmed.ibrahim.yassen@gmail.com>
2022-07-17 18:35:18 +0200

    Finish sendCancelationEmail function

--- emails/emailHandler.js
+++ emails/emailHandler.js
@@ -13,3 +13,12 @@ exports.sendWelcomeEmail = (userEmail, userName) => {
     text: `Welcome to the app, ${userName}!\n\tLet me show you how to use the App:`,
   });
 };
+
+exports.sendCancelationEmail = (userEmail, userName) => {
+  sgMail.send({
+    to: userEmail,
+    from: "ahmedibrahim9629@gmail.com",
+    subject: "Sorry to see you go!",
+    text: `Your feedback is important to us, ${userName}!\n\tPlease let us know how we could improve our services:`,
+  });
+};
