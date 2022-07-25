ef0fc7af4d63cbd02afe992e5c6bc5df2ab7b89e
Ahmed I. Yassen <ahmed.ibrahim.yassen@gmail.com>
2022-07-25 10:25:01 +0200

    Setup testing environment & folder structure

--- src/app.js
+++ src/app.js
@@ -4,15 +4,12 @@ const mongoose = require("mongoose");
 const userRoute = require("../routes/userRoute");
 const taskRoute = require("../routes/taskRoute");
 
-/********** Run Server ***********/
+/********** Run DB Server ***********/
 const app = express();
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => {
-    const port = process.env.PORT;
-    app.listen(port, () => {
-      console.log(`Server is up on port ${port}!`);
-    });
+    console.log("Connected to DB Successfully!");
   })
   .catch((e) => console.log("DB Error! " + e));
 
@@ -33,3 +30,5 @@ app.use((error, req, res, next) => {
     .status(error.status || 500)
     .json({ msg: `${error.status || 500} Internal Server Error! ${error}` });
 });
+
+module.exports = app;
